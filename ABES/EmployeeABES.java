91582d04cd405a1532e52dcf6779fe1f55130c26
Sneha Sahu <sneha@example.com>
2025-08-24 15:49:56 +0530

    Initial commit

---
+++ ABES/EmployeeABES.java
@@ -0,0 +1,15 @@
+package ABES;
+
+import KIET.*;
+
+public class EmployeeABES {
+    public static void main(String[] args) {
+        EmployeeKIET ek=new EmployeeKIET();
+        ek.Emp_name="Kinjal"; 
+        ek.Emp_id=3; 
+        ek.Salary=150000; 
+        ek.Organisation="KIET ABES"; 
+
+    }
+    
+}
