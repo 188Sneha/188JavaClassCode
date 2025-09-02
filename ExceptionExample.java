9842ea2e5c1c0be05a4613e59fce29e2558bb43c
Sneha Sahu <sneha@example.com>
2025-09-02 21:53:38 +0530

    02-09-2025

---
+++ ExceptionExample.java
@@ -0,0 +1,20 @@
+public class ExceptionExample {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(100/0);    //arithmetic runtime check
+        System.out.println(5);
+        System.out.println(6);
+
+        try{
+            System.out.println(100/0);
+        }
+        catch(Exception e){
+            System.out.println(5);
+            System.out.println(6);
+        }
+
+    }
+}
